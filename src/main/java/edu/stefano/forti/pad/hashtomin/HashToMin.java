cb77a724b0c4e1eb08b79faf81788a269ec9b395
stefano <stefano.forti92@gmail.com>
2016-03-11 17:46:45 +0100

    iterative behaviour OK

--- src/main/java/edu/stefano/forti/pad/hashtomin/HashToMin.java
+++ src/main/java/edu/stefano/forti/pad/hashtomin/HashToMin.java
@@ -6,25 +6,24 @@
 package edu.stefano.forti.pad.hashtomin;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Counters;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.*;
 
-enum StopCondition {
-    MERGED
-}
+
 
 /**
  *
  * @author stefano
  */
-public class HashToMin extends Configured implements Tool {
+public class HashToMin extends BaseJob {
 
     enum StopCondition {
         MERGED
@@ -37,53 +36,84 @@ public class HashToMin extends Configured implements Tool {
      * @throws java.lang.ClassNotFoundException
      */
     public static void main(String[] args) throws Exception {
-        int exitCode = ToolRunner.run(new Configuration(),new HashToMin(), args);
+        int exitCode = ToolRunner.run(new Configuration(), new HashToMin(), args);
+        if (args.length != 2) {
+            System.err.println("Usage: <in> <output name> ");
+        }
         System.exit(exitCode);
-        
+
+    }
+
+    private Job getJobConf(String[] args) throws Exception {
+
+        JobInfo jobInfo = new JobInfo() {
+            @Override
+            public Class<? extends Reducer> getCombinerClass() {
+                return null;
+            }
+
+            @Override
+            public Class<?> getJarByClass() {
+                return HashToMin.class;
+            }
+
+            @Override
+            public Class<? extends Mapper> getMapperClass() {
+                return HashToMinMapper.class;
+            }
+
+            @Override
+            public Class<?> getOutputKeyClass() {
+                return IntWritable.class;
+            }
+
+            @Override
+            public Class<?> getOutputValueClass() {
+                return Text.class;
+            }
+
+            @Override
+            public Class<? extends Reducer> getReducerClass() {
+                return HashToMinReducer.class;
+            }
+        };
+
+        return setupJob("hashtomin", jobInfo);
+
     }
 
     @Override
     public int run(String[] strings) throws Exception {
-        
-        Job job = new Job();
-        job.setJarByClass(HashToMin.class);
-        job.setJobName("To Adjacency Lists");
-        job.setMapperClass(HashToMinMapper.class);
-        job.setReducerClass(HashToMinReducer.class);
-        job.setNumReduceTasks(1);
 
-        job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(ClusterWritable.class);
-
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(Text.class);
+        Job job;
         long iterate = 1;
         int iterations = 0;
-        boolean exitCode = false;
-        String input, output = null;
-     
         
-        while (iterate > 0){
-            
-        if (iterations == 0){
-            input = strings[0];
-        } else
-        {
-            input = strings[1]+iterations;
+
+        while (iterate > 0) {
+            job = getJobConf(strings);
+            String input, output;
+
+            if (iterations == 0) {
+                input = strings[0];
+            } else {
+                input = strings[1] + iterations;
+            }
+
+            output = strings[1] + (iterations + 1);
+            FileInputFormat.setInputPaths(job, new Path(input));
+            FileOutputFormat.setOutputPath(job, new Path(output));
+            job.setMapOutputKeyClass(IntWritable.class);
+            job.setMapOutputValueClass(ClusterWritable.class);
+            job.waitForCompletion(true);
+
+            Counters counters = job.getCounters();
+            iterate = counters.findCounter(StopCondition.MERGED).getValue();
+            counters.findCounter(StopCondition.MERGED).setValue(0);
+            iterations++;
         }
-        output = strings[1] + (iterations + 1);
-        FileInputFormat.addInputPath(job, new Path(input));
-        FileOutputFormat.setOutputPath(job, new Path(output));
-        exitCode=job.waitForCompletion(true);
-        
-        Counters counters = job.getCounters();
-        iterate = counters.findCounter(StopCondition.MERGED).getValue();
-        counters.findCounter(StopCondition.MERGED).setValue(0);
-        iterations++;
-        }
-   
-        
-        return exitCode ? 0 : 1;
+
+        return 0;
     }
 
 }
--- src/main/java/edu/stefano/forti/pad/hashtomin/HashToMinReducer.java
+++ src/main/java/edu/stefano/forti/pad/hashtomin/HashToMinReducer.java
@@ -5,15 +5,13 @@
  */
 package edu.stefano.forti.pad.hashtomin;
 
+import edu.stefano.forti.pad.hashtomin.HashToMin.StopCondition;
 import java.io.IOException;
 import java.util.TreeSet;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-
-
-
 
 /**
  *
@@ -24,19 +22,20 @@ public class HashToMinReducer extends Reducer<IntWritable, ClusterWritable, IntW
     @Override
     public void reduce(IntWritable vertex, Iterable<ClusterWritable> clusters, Context context)
             throws IOException, InterruptedException {
-        
+
         TreeSet<Integer> cluster = new TreeSet<Integer>();
         int i;
-        
+
         //update C_v in (v,C_v)
-        for (ClusterWritable c : clusters) {      
+        for (ClusterWritable c : clusters) {
             boolean changed = cluster.addAll(c.get());
-            if (changed)
-               context.getCounter(StopCondition.MERGED).increment(1);
+            if (changed) {
+                context.getCounter(StopCondition.MERGED).increment(1);
+            }
         }
 
         //writes updated (u,C_u) to file
-        context.write(vertex,new Text(new ClusterWritable(cluster).toString()));
+        context.write(vertex, new Text(new ClusterWritable(cluster).toString()));
     }
 
 }
