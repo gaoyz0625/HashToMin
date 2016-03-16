3e2700df7138091a4c7b2b99ede03820187dad6c
stefano <stefano.forti92@gmail.com>
2016-03-16 10:13:53 +0100

    verifier map/red

--- src/main/java/edu/stefano/forti/pad/hashtomin/HashToMinReducer.java
+++ src/main/java/edu/stefano/forti/pad/hashtomin/HashToMinReducer.java
@@ -1,21 +1,28 @@
-/**
-The MIT License (MIT)
-
-Copyright (c) 2016 Stefano Forti
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-**/
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Stefano Forti
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package edu.stefano.forti.pad.hashtomin;
 
-import edu.stefano.forti.pad.hashtomin.HashToMin.StopCondition;
 import java.io.IOException;
 import java.util.TreeSet;
 
@@ -41,7 +48,7 @@ public class HashToMinReducer extends Reducer<IntWritable, ClusterWritable, IntW
         }
         //checks whether there has been convergence to <u, {v_min}> and <v_min, C> couples
         if (cluster.size() > 1 && v > cluster.first()) {
-                context.getCounter(StopCondition.GO_ON).increment(1);
+                context.getCounter(JobCounters.GO_ON).increment(1);
             }
         
         //writes updated (u,C_u) 
