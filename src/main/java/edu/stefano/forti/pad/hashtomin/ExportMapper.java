4e6a986895de6c9c5c00864b807129442a8f0630
stefano <stefano.forti92@gmail.com>
2016-03-16 09:54:55 +0100

    license

--- src/main/java/edu/stefano/forti/pad/hashtomin/ExportMapper.java
+++ src/main/java/edu/stefano/forti/pad/hashtomin/ExportMapper.java
@@ -1,8 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+The MIT License (MIT)
+
+Copyright (c) 2016 Stefano Forti
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+**/
 package edu.stefano.forti.pad.hashtomin;
 
 import java.io.IOException;
